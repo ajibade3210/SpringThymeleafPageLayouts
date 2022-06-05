a632e294cca7112a7e2775e69d7f855c0be53cfc
ajibade3210 <ajibade69th@gmail.com>
2022-06-05 14:59:25 +0100

    layout creation

---
+++ IncludeStyleLayouts/src/main/java/com/longbridge/IncludeStyleLayouts.java
@@ -0,0 +1,13 @@
+package com.longbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncludeStyleLayouts {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncludeStyleLayouts.class, args);
+    }
+
+}
